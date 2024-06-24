75fdeb0ed13129fb3d4a4fbc35281bdd813fbbb4
Shoxrux Rasulov <shokhrukhrasulov07@gmail.com>
2024-06-24 21:41:02 +0500

    updated project add cart and new animation

--- lib/views/screens/online_market_screen.dart
+++ lib/views/screens/online_market_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class OnlineMarketScreen extends StatefulWidget {
@@ -8,14 +10,51 @@ class OnlineMarketScreen extends StatefulWidget {
 }
 
 class _OnlineMarketScreenState extends State<OnlineMarketScreen> {
+  List carts = [
+    "assets/images/humo_cart.png",
+    "assets/images/kdb_cart.png",
+    "assets/images/ravnaq_cart.png",
+    "assets/images/davr_cart.png",
+    "assets/images/xalq_cart.png",
+  ];
+
+  late PageController _pageController;
+  late Timer _timer;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+    _timer = Timer.periodic(const Duration(seconds: 2), (Timer timer) {
+      if (_currentPage < carts.length) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 1000),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[200],
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue[200],
         title: const Text(
-          "Online Market",
+          "Cart Shop",
           style: TextStyle(
             fontFamily: 'Lato',
             color: Colors.white,
@@ -25,19 +64,32 @@ class _OnlineMarketScreenState extends State<OnlineMarketScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
+            child: PageView.builder(
+              controller: _pageController,
               scrollDirection: Axis.horizontal,
-              itemCount: 6,
+              itemCount: carts.length,
               itemBuilder: (ctx, index) {
-                return Card(
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
                   child: Container(
                     width: 200,
                     height: 100,
-                    color: Colors.amber,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(carts[index]),
+                      ),
+                    ),
                   ),
                 );
               },
             ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(),
           ),
         ],
       ),
